bcbc9ada0e06307f87c5a7843693725917d0def0
Jane Curry <jane.curry@skills-1st.co.uk>
2012-04-10 17:20:03 +0100

    Version 1.3 - minor update to remove energy_days template

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.ZenSystems.olsonPower"
-VERSION = "1.2"
+VERSION = "1.3"
 AUTHOR = "Jane Curry"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.ZenSystems']
